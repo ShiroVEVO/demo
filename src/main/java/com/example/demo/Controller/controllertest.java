d9664d1adc9fc9e279a141f027738db0cb54f9bb
Adrianシロ <adrianstratos@gmail.com>
2024-06-06 13:09:25 -0500

    demo 1

---
+++ src/main/java/com/example/demo/Controller/controllertest.java
@@ -0,0 +1,19 @@
+package com.example.demo.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/testeo")
+public class controllertest {
+
+    @GetMapping
+    public ResponseEntity<String> getAllAcounts() {
+        return new ResponseEntity<String>("oc", null, 200);
+    }
+}
